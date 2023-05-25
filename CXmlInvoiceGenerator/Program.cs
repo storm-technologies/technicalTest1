e224ce9be7923ae47346ec959bf29db2bccc14d4
Marcin Malinowski <marcin.malinowski@outlook.com>
2023-05-25 10:05:55 +0100

    This is one possible apprach to generate something like that. I  have added some info and explanations  in comments in code. Kind Regards, Marcin.

--- CXmlInvoiceGenerator/Program.cs
+++ CXmlInvoiceGenerator/Program.cs
@@ -1,5 +1,7 @@
 ﻿using DatabaseAccess;
 using System.Data;
+using System.Xml;
+using System.Xml.Serialization;
 
 namespace CXmlInvoiceGenerator
 {
@@ -13,50 +15,271 @@ namespace CXmlInvoiceGenerator
             Console.WriteLine("New invoice generation run finishing at " + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"));
         }
 
-
         private static void GenerateCXMLForNewInvoices()
         {
+            Invoices invoices = new Invoices();
+            DataTable newInvoicesTable = invoices.GetNewInvoices();
+            int i = 1;
 
-            // == Please complete this function ==
+            var cxml = new Cxml
+            {
+                Version = "1.0",
+                PayloadID = "xxx.xxxx@example.coupahost.com",
+                Timestamp = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"),
 
-            // 1) Using the DatabaseAccess dll provided and referenced (in the refs folder), load each invoice from the database
-            //
-            // 2) Create a cXml invoice document using the information from each invoice
+            };
 
-            // The following is a very helpful resource for cXml:
+           
+            foreach (DataRow invoiceRow in newInvoicesTable.Rows)
+            {
+                int invoiceId = Convert.ToInt32(invoiceRow["Id"]);
+                string? CurrCode = invoiceRow["CurrencyCode"].ToString();
+                var InvoiceDetailItems = new List<InvoiceDetailItem>();
+                DataTable itemsTable = invoices.GetItemsOnInvoice(invoiceId);
+                DataRow deliveryAddressTable = invoices.GetDeliveryAddressForSalesOrder(Convert.ToInt32(invoiceRow["SalesOrderId"]));
+                DataRow billingAddressTable = invoices.GetBillingAddressForInvoice(invoiceId);
 
-            // https://compass.coupa.com/en-us/products/product-documentation/supplier-resources/supplier-integration-resources/standard-invoice-examples/sample-cxml-invoice-with-payment-terms
+                GenerateInvoiceDetailItems(CurrCode, InvoiceDetailItems, itemsTable);
 
-            // Assume the invoice is raised on the same day you find it, so PaymentTerms is from Today
+                Console.WriteLine($"Generating Invoice Detail Items structures for invoice no {i} completed at {DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")}");
 
-            // VAT mode is header (overall total) only, not at item level
+                AddHeader(cxml);
 
-            // 3) Save the created invoices into a specified output file with the .xml file extension
+                Console.WriteLine($"Generating Request structure for invoice no {i} started at {DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")}");
+                //way too many params, but I decided to use bulilt in extract method function here for sake of everything being easier to read
+                GenerateRequest(cxml, invoiceRow, invoiceId, CurrCode, InvoiceDetailItems, deliveryAddressTable, billingAddressTable);
+                
+                Console.WriteLine($"Generating Request structure for invoice no {i} completed at {DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")}");
 
-            // The "purpose" for each invoice is "standard"
-            // The "operation" for each invoice is "new"
-            // The output folder is entirely up to you, based on your file system
-            // You can use "fake" credentials (Domain/Identity/SharedSecret etc. etc.) of your own choosing for the From/To/Sender section for this test
-            //
-            // It would likely be a good idea for all of these to be configurable in some way, in a .Net options/settings file or an external ini file
+                SerializeCXML(i, cxml);
+                i++;
+            }
 
-            // Ideally, you will write reasonable progress steps to the console window
 
-            // You may add references to anything you want from the standard Nuget URL
+        }
+        //It is entirely possible that I got some mappings wrong during creation of this structure, but that would be a general idea
+        private static void GenerateRequest(Cxml cxml, DataRow invoiceRow, int invoiceId, string? CurrCode, List<InvoiceDetailItem> InvoiceDetailItems, DataRow deliveryAddressTable, DataRow billingAddressTable)
+        {
+            cxml.Request = new Request
+            {
+                DeploymentMode = "Test",
+                InvoiceDetailRequest = new InvoiceDetailRequest
+                {
+                    InvoiceDetailRequestHeader = new InvoiceDetailRequestHeader
+                    {
+                        InvoiceID = invoiceId.ToString(),
+                        Purpose = "standard",
+                        Operation = "new",
+                        InvoiceDate = DateTime.Now.ToString(),
+                        InvoiceDetailHeaderIndicator = new InvoiceDetailHeaderIndicator(),
+                        InvoiceDetailLineIndicator = new InvoiceDetailLineIndicator { IsAccountingInLine = "yes" },
+                        InvoicePartner = new InvoicePartner[] {
+                                new InvoicePartner
+                                {
+                                    Contact = new Contact
+                                    {
+                                        Role = "soldTo",
+                                        Name = deliveryAddressTable["ContactName"].ToString() ?? "",
+                                        PostalAddress = new PostalAddress
+                                        {
+                                            Street = deliveryAddressTable["AddrLine1"].ToString() ?? "",
+                                            City = deliveryAddressTable["AddrLine2"].ToString() ?? "",
+                                            State = deliveryAddressTable["AddrLine3"].ToString() ?? "",
+                                            PostalCode = deliveryAddressTable["AddrPostCode"].ToString() ?? "",
+                                            Country = new Country { IsoCountryCode = deliveryAddressTable["CountryCode"].ToString() ?? "", Value = deliveryAddressTable["Country"].ToString() ?? "" }
+                                        }
+                                    }
+                                }, new InvoicePartner
+                                {
+                                    Contact = new Contact
+                                    {
+                                        Role = "billTo",
+                                        Name = billingAddressTable["ContactName"].ToString() ?? "",
+                                        PostalAddress = new PostalAddress
+                                        {
+                                            Street = billingAddressTable["AddrLine2"].ToString() ?? "",
+                                            City = billingAddressTable["AddrLine3"].ToString() ?? "",
+                                            State = billingAddressTable["AddrLine4"].ToString() ?? "",
+                                            PostalCode = billingAddressTable["AddrPostCode"].ToString() ?? "",
+                                            Country = new Country { IsoCountryCode = billingAddressTable["CountryCode"].ToString() ?? "", Value = billingAddressTable["Country"].ToString() ?? "" }
+                                        }
+                                    }
+                                } },
 
-            // You may modify the signature to this function if you want to pass values into it
 
-            // You may move this code into another class (or indeed classes) of your choosing
 
-            Invoices invoiceDB = new();
-            DataTable newInvoices = invoiceDB.GetNewInvoices();
+                        PaymentTerms = new PaymentTerm
+                        {
+                            PayInNumberOfDays = Convert.ToInt32(invoiceRow["PaymentTermsDays"]), 
 
-            // invoiceDB contains other functions you will need...
+                        }
+                    },
+                    InvoiceDetailOrder = new InvoiceDetailOrder
+                    {
+                        InvoiceDetailItem = InvoiceDetailItems.ToArray(),
+                        InvoiceDetailOrderInfo = new InvoiceDetailOrderInfo { OrderReference = new OrderReference { DocumentReference = new DocumentReference { PayloadID = "PayLoadId" } } }
+                    },
+                    InvoiceDetailSummary = new InvoiceDetailSummary
+                    {
+                        SubtotalAmount = new SubtotalAmount { Money = new Money { Currency = CurrCode, Value = "val" } },
+                        Tax = new Tax
+                        {
 
+                            Money = new Money { Currency = "" },
+                            Description = "Desc",
+                            TaxDetail = new TaxDetail
+                            {
+                                Purpose = "Tax",
+                                Category = "sales",
+                                PercentageRate = Convert.ToInt32(invoiceRow["VATPercentage"]),
+
+                                TaxableAmount = new TaxableAmount { Money = new Money { Currency = CurrCode, Value = invoiceRow["NetAmount"].ToString() } },
+                                TaxAmount = new TaxAmount { Money = new Money { Currency = CurrCode, Value = invoiceRow["VATAmount"].ToString() } },
+                                TaxLocation = "GB"
+                            }
+
+                        },
+                        ShippingAmount = new ShippingAmount
+                        {
+                            Money = new Money { Currency = CurrCode, Value = "ShippingAmount" }
+                        },
+
+                        GrossAmount = new GrossAmount
+                        {
+                            Money = new Money { Currency = CurrCode, Value = invoiceRow["GrossAmount"].ToString() }
+                        },
+
+                        NetAmount = new NetAmount
+                        {
+                            Money = new Money { Currency = CurrCode, Value = invoiceRow["NetAmount"].ToString() }
+                        },
+
+                        DueAmount = new DueAmount
+                        {
+                            Money = new Money { Currency = CurrCode, Value = "DueAmount" }
+                        },
+                    },
+
+                }
+
+            };
+        }
+
+        private static void AddHeader(Cxml cxml)
+        {
+            cxml.Header = new Header
+            {
+                From = new From
+                {
+                    Credential = new Credential
+                    {
+                        Domain = "DUNS",
+                        Identity = "xxxxxxxx"
+                    }
+                },
+                To = new To
+                {
+                    Credential = new Credential
+                    {
+                        Domain = "NetworkID",
+                        Identity = "yyyyyyyy"
+                    }
+                },
+                Sender = new Sender
+                {
+                    Credential = new Credential
+                    {
+                        Domain = "DUNS",
+                        Identity = "xxxxxxxxx",
+                        SharedSecret = "xxxxxxxxx"
+                    },
+                    UserAgent = "Coupa Procurement 1.0"
+                }
+            };
+        }
+
+        private static void GenerateInvoiceDetailItems(string CurrCode, List<InvoiceDetailItem> InvoiceDetailItems, DataTable itemsTable)
+        {
+            int iteration = 0;
+            foreach (DataRow itemRow in itemsTable.Rows)
+            {
+                iteration++;
+                Console.WriteLine($"Generating Invoice detail Item structure no {iteration}  at {DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")} ");
+
+                InvoiceDetailItems.Add(new InvoiceDetailItem()
+                {
+                    InvoiceLineNumber = Convert.ToDecimal(itemRow["LineTotal"]),
+                    Quantity = Convert.ToInt32(itemRow["Qty"]),
+                    UnitOfMeasure = "EA",
+                    UnitPrice = new UnitPrice { Money = new Money { Currency = CurrCode, Value = itemRow["UnitPrice"].ToString() } },
+                    InvoiceDetailItemReference = new InvoiceDetailItemReference
+                    {
+                        LineNumber = Convert.ToDecimal(itemRow["LineTotal"]),
+                        ItemID = new ItemID { SupplierPartID = itemRow["ID"].ToString() },
+                        Description = itemRow["Description"].ToString(),
+                        ManufacturerPartID = itemRow["PartNo"].ToString(),
+                        ManufacturerName = itemRow["Manufacturer"].ToString()
+                    }
+                });
+            };
+        }
+
+        //where i is invoice iterator, I have decided to provide each invoice in separate file
+        private static void SerializeCXML(int i, Cxml cxml)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(Cxml));
+            using (StringWriter writer = new StringWriter())
+            {
+                string filename = String.Concat("InvoiceRequest", i.ToString(), ".xml");
+                Console.WriteLine($"XML serialization for invoice {i} starting at {DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")}");
+                serializer.Serialize(writer, cxml);
+                string xml = writer.ToString();
+                // Save the XML to a file
+
+                /*the assumption is that few things should be configurable however I have an issue with my IDE and despite
+                 * added reference to System.Configuration.ConfigurationManager I somehow can't add app.config to my solution
+                 * The idea would be to put there (among others) a path to output dir and then in code retrieve it by
+                 * string path = ConfigurationManager.AppSettings["OutputPath"];
+                 * But I don't have time to diagnose it further on
+                 */
+
+                File.WriteAllText(filename, xml);
+                Console.WriteLine($"XML serialization for invoice {i} completed at {DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")}");
+                Console.WriteLine($"Output for invoice {i} succesfully saved to a file: {filename}");
+            }
         }
 
 
+        // == Please complete this function ==
 
+        // 1) Using the DatabaseAccess dll provided and referenced (in the refs folder), load each invoice from the database
+        //
+        // 2) Create a cXml invoice document using the information from each invoice
+
+        // The following is a very helpful resource for cXml:
+
+        // https://compass.coupa.com/en-us/products/product-documentation/supplier-resources/supplier-integration-resources/standard-invoice-examples/sample-cxml-invoice-with-payment-terms
+
+        // Assume the invoice is raised on the same day you find it, so PaymentTerms is from Today
+
+        // VAT mode is header (overall total) only, not at item level
+
+        // 3) Save the created invoices into a specified output file with the .xml file extension
+
+        // The "purpose" for each invoice is "standard"
+        // The "operation" for each invoice is "new"
+        // The output folder is entirely up to you, based on your file system
+        // You can use "fake" credentials (Domain/Identity/SharedSecret etc. etc.) of your own choosing for the From/To/Sender section for this test
+        //
+        // It would likely be a good idea for all of these to be configurable in some way, in a .Net options/settings file or an external ini file
+
+        // Ideally, you will write reasonable progress steps to the console window
+
+        // You may add references to anything you want from the standard Nuget URL
+
+        // You may modify the signature to this function if you want to pass values into it
+
+        // You may move this code into another class (or indeed classes) of your choosing
 
     }
 }
---
+++ CXmlInvoiceGenerator/SerializationClasses.cs
@@ -0,0 +1,304 @@
+﻿using System;
+using System.Xml.Serialization;
+
+/*File contains all the classes that will be used for generating CXML output*/
+
+
+namespace CXmlInvoiceGenerator
+{
+    [XmlRoot(ElementName = "cXML", Namespace = "http://xml.cxml.org/schemas/cXML/1.2/InvoiceDetail.dtd")]
+    public class Cxml
+    {
+        [XmlAttribute(AttributeName = "version")]
+        public string Version { get; set; }
+
+        [XmlAttribute(AttributeName = "payloadID")]
+        public string PayloadID { get; set; }
+
+        [XmlAttribute(AttributeName = "timestamp")]
+        public string Timestamp { get; set; }
+
+        public Header Header { get; set; }
+        public Request Request { get; set; }
+    }
+
+    public class Header
+    {
+        public From From { get; set; }
+        public To To { get; set; }
+        public Sender Sender { get; set; }
+    }
+
+    public class From
+    {
+        public Credential Credential { get; set; }
+    }
+
+    public class To
+    {
+        public Credential Credential { get; set; }
+    }
+
+    public class Sender
+    {
+        public Credential Credential { get; set; }
+
+        [XmlElement(ElementName = "UserAgent")]
+        public string UserAgent { get; set; }
+    }
+
+    public class Credential
+    {
+        [XmlAttribute(AttributeName = "domain")]
+        public string Domain { get; set; }
+
+        public string Identity { get; set; }
+
+        [XmlElement(ElementName = "SharedSecret")]
+        public string SharedSecret { get; set; }
+    }
+
+    public class Request
+    {
+        [XmlAttribute(AttributeName = "deploymentMode")]
+        public string DeploymentMode { get; set; }
+
+        public InvoiceDetailRequest InvoiceDetailRequest { get; set; }
+    }
+
+    public class InvoiceDetailRequest
+    {
+        public InvoiceDetailRequestHeader InvoiceDetailRequestHeader { get; set; }
+        public InvoiceDetailOrder InvoiceDetailOrder { get; set; }
+        public InvoiceDetailSummary InvoiceDetailSummary { get; set; }
+    }
+
+    public class InvoiceDetailRequestHeader
+    {
+        [XmlAttribute(AttributeName = "invoiceID")]
+        public string InvoiceID { get; set; }
+
+        [XmlAttribute(AttributeName = "purpose")]
+        public string Purpose { get; set; }
+
+        [XmlAttribute(AttributeName = "operation")]
+        public string Operation { get; set; }
+
+        [XmlAttribute(AttributeName = "invoiceDate")]
+        public string InvoiceDate { get; set; }
+
+        public InvoiceDetailHeaderIndicator InvoiceDetailHeaderIndicator { get; set; }
+        public InvoiceDetailLineIndicator InvoiceDetailLineIndicator { get; set; }
+        public InvoicePartner[] InvoicePartner { get; set; }
+        public PaymentTerm PaymentTerms { get; set; }
+    }
+
+    public class InvoiceDetailHeaderIndicator { }
+
+    public class InvoiceDetailLineIndicator
+    {
+        [XmlAttribute(AttributeName = "isAccountingInLine")]
+        public string IsAccountingInLine { get; set; }
+    }
+
+    public class InvoicePartner
+    {
+        public Contact Contact { get; set; }
+    }
+
+    public class Contact
+    {
+        [XmlAttribute(AttributeName = "role")]
+        public string Role { get; set; }
+        public string AddressID { get; set; }
+        public string Name { get; set; }
+        public PostalAddress PostalAddress { get; set; }
+    }
+
+    public class PostalAddress
+    {
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string PostalCode { get; set; }
+        public Country Country { get; set; }
+    }
+
+    public class Country
+    {
+        [XmlAttribute(AttributeName = "isoCountryCode")]
+        public string IsoCountryCode { get; set; }
+
+        [XmlText]
+        public string Value { get; set; }
+    }
+
+    public class PaymentTerm
+    {
+        [XmlAttribute(AttributeName = "payInNumberofDays")]
+        public int PayInNumberOfDays;
+
+        [XmlElement(ElementName = "Discount")]
+        public Discount Discount { get; set; }
+
+        public int NetDueDays { get; set; }
+    }
+
+    public class Discount
+    {
+        public DiscountPercent DiscountPercent { get; set; }
+        public int DiscountDueDays { get; set; }
+    }
+
+    public class DiscountPercent
+    {
+        [XmlAttribute(AttributeName = "percent")]
+        public int Percent { get; set; }
+    }
+
+    public class InvoiceDetailOrder
+    {
+        public InvoiceDetailOrderInfo InvoiceDetailOrderInfo { get; set; }
+        public InvoiceDetailItem[] InvoiceDetailItem { get; set; }
+    }
+
+    public class InvoiceDetailOrderInfo
+    {
+        public OrderReference OrderReference { get; set; }
+    }
+
+    public class OrderReference
+    {
+        public DocumentReference DocumentReference { get; set; }
+    }
+
+    public class DocumentReference
+    {
+        [XmlAttribute(AttributeName = "payloadID")]
+        public string PayloadID { get; set; }
+    }
+
+    public class InvoiceDetailItem
+    {
+        [XmlAttribute(AttributeName = "invoiceLineNumber")]
+        public decimal InvoiceLineNumber { get; set; }
+
+        [XmlAttribute(AttributeName = "quantity")]
+        public int Quantity { get; set; }
+
+        public string UnitOfMeasure { get; set; }
+        public UnitPrice UnitPrice { get; set; }
+        public InvoiceDetailItemReference InvoiceDetailItemReference { get; set; }
+        public SubtotalAmount SubtotalAmount { get; set; }
+        public GrossAmount GrossAmount { get; set; }
+        public NetAmount NetAmount { get; set; }
+    }
+
+    public class UnitPrice
+    {
+        public Money Money { get; set; }
+    }
+
+    public class InvoiceDetailItemReference
+    {
+        [XmlAttribute(AttributeName = "lineNumber")]
+        public decimal LineNumber { get; set; }
+
+        public ItemID ItemID { get; set; }
+
+        [XmlElement(ElementName = "Description")]
+        public string Description { get; set; }
+
+        public string ManufacturerPartID { get; set; }
+
+        [XmlElement(ElementName = "ManufacturerName")]
+        public string ManufacturerName { get; set; }
+    }
+
+    public class ItemID
+    {
+        public string SupplierPartID { get; set; }
+    }
+
+    public class SubtotalAmount
+    {
+        public Money Money { get; set; }
+    }
+
+    public class GrossAmount
+    {
+        public Money Money { get; set; }
+    }
+
+    public class NetAmount
+    {
+        public Money Money { get; set; }
+    }
+
+    public class InvoiceDetailSummary
+    {
+        public SubtotalAmount SubtotalAmount { get; set; }
+        public Tax Tax { get; set; }
+        public ShippingAmount ShippingAmount { get; set; }
+        public GrossAmount GrossAmount { get; set; }
+        public NetAmount NetAmount { get; set; }
+        public DueAmount DueAmount { get; set; }
+    }
+
+    public class Tax
+    {
+        public Money Money { get; set; }
+
+        [XmlElement(ElementName = "Description")]
+        public string Description { get; set; }
+
+        public TaxDetail TaxDetail { get; set; }
+    }
+
+    public class TaxDetail
+    {
+        [XmlAttribute(AttributeName = "purpose")]
+        public string Purpose { get; set; }
+
+        [XmlAttribute(AttributeName = "category")]
+        public string Category { get; set; }
+
+        [XmlAttribute(AttributeName = "percentageRate")]
+        public int PercentageRate { get; set; }
+
+        public TaxableAmount TaxableAmount { get; set; }
+        public TaxAmount TaxAmount { get; set; }
+
+        [XmlElement(ElementName = "TaxLocation")]
+        public string TaxLocation { get; set; }
+    }
+
+    public class TaxableAmount
+    {
+        public Money Money { get; set; }
+    }
+
+    public class TaxAmount
+    {
+        public Money Money { get; set; }
+    }
+
+    public class ShippingAmount
+    {
+        public Money Money { get; set; }
+    }
+
+    public class DueAmount
+    {
+        public Money Money { get; set; }
+    }
+
+    public class Money
+    {
+        [XmlAttribute(AttributeName = "currency")]
+        public string Currency { get; set; }
+
+        [XmlText]
+        public string Value { get; set; }
+    }
+}
